d617fb0251327156b42098a8baa2a15ab8a00613
Pavan Kumar <pavankumar7860@gmail.com>
2024-09-13 16:19:13 +0530

    Feature/add shadcn UI (#2)
    * Adding shadcn and changing approach for lint stage config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 
 export default function Home() {
@@ -50,7 +51,7 @@ export default function Home() {
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        Footer
+        <Button variant="outline">Footer</Button>
       </footer>
     </div>
   );
